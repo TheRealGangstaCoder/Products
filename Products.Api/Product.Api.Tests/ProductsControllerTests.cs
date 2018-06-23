fbce1c5d972e75b4f7a479a558819569a398db72
JoshiPranav <joshpranav@gmail.com>
2018-06-23 14:13:29 +1000

    tests

---
+++ Products.Api/Product.Api.Tests/ProductsControllerTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Products.Api.Controllers;
+using System.Linq;
+
+namespace Product.Api.Tests
+{
+    [TestClass]
+    public class ProductsControllerTests
+    {
+        [TestMethod]
+        public void Should_Return_2_Products()
+        {
+            var prodController = new ProductsController();
+            var products = prodController.Get();
+            Assert.IsTrue(products.Count() == 2);
+        }
+    }
+}
